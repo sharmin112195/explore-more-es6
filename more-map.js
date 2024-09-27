eb7fd0a7402c7fee9a9ec130ecc03194801f7d28
shopna's Macbook <sharminshopna149@gmail.com>
2024-09-27 20:49:26 +0600

    more map file done

---
+++ more-map.js
@@ -0,0 +1,13 @@
+const numbers = [12, 10, 8, 15, 7];
+const doubled = numbers.map(num => num * 2);
+// console.log(doubled)
+const fiveBonus = numbers.map(num => num + 5);
+// console.log(fiveBonus);
+const halves = numbers.map(num => num / 2);
+console.log(halves);
+
+const friends = ['Tom', 'John', 'Micheal', 'Alve'];
+const lengths = friends.map(frnd => frnd.length)
+console.log(lengths);
+const firstLetter = friends.map(friend => friend[0])
+console.log(firstLetter);
