a70f7adc962804509aa7130529433c02777f1da8
shopna's Macbook <sharminshopna149@gmail.com>
2024-09-27 20:10:06 +0600

    map file done

---
+++ map.js
@@ -0,0 +1,23 @@
+// map ==> loops through each element of the array and do the operation that you passed in the call back function and hold the result from each operation in an array and finally returns you the array
+
+const numbers = [4, 5, 2, 8, 10];
+// const doubled =[];
+// for(const num of numbers){
+//     const double = num * 2;
+//     doubled.push(double);
+// }
+// console.log(doubled);
+
+function doubleIt (num){
+    // console.log('num now', num)
+    return num * 2;
+}
+const result = numbers.map(doubleIt);
+// console.log(result);
+
+const double2 = n => n * 2;
+const output = numbers.map(double2)
+// console.log(output)
+const output2 = numbers.map(n => n*2)
+console.log(output2)
+
