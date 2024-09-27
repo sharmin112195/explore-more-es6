c47a8ddeba0b84cd3e6c16e585b0317a56cef778
shopna's Macbook <sharminshopna149@gmail.com>
2024-09-27 19:33:37 +0600

    chain file compleat

--- chain.js
+++ chain.js
@@ -1,1 +1,37 @@
 // data access
+const data = [{id:1, name: 'abul', address: 'jeshore'}];
+console.log(data[0].address)
+
+const products = {
+    count : 5000,
+    data : [
+        {id:1, name:'lenovo laptop', price: 65000},
+        {id:2, name:'macbook', price: 165000},
+    ]
+}
+console.log(products.data[1].price)
+
+const user = {
+    id: 5001,
+    name: 'Shoriful Raj',
+    address:{
+        street:{
+            first: '54/1 uttor side',
+            second: 'poribag er goli',
+            third : 'no dorai'
+        },
+        city: 'Dhaka'
+    }
+}
+// console.log(user.address.street.second);
+console.log(user.address.street?.second);
+
+const user2 = {
+    id: 5002,
+    name: 'pori bibir majar',
+    address:{
+        city:'Chittagong',
+        country: 'Bangladesh'
+    }
+}
+console.log(user2.address.country)
